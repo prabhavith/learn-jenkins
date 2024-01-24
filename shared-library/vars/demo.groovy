07fc7e0d85719c1e335cae433081b483bdf983d0
prabhavith <you@example.com>
2024-01-24 08:48:09 +0530

    pushed jenkins file

---
+++ shared-library/vars/demo.groovy
@@ -0,0 +1,27 @@
+def call() {
+    pipeline {
+        agent { node { label 'workstation' } }
+        stages {
+            stage('compile') {
+                steps {
+                    echo 'compile'
+                }
+            }
+            stage('testcases') {
+                steps {
+                    echo 'testcases'
+                }
+            }
+            stage('build') {
+                steps {
+                    echo 'build'
+                }
+            }
+            stage('release'){
+                steps{
+                    echo 'release'
+                }
+            }
+        }
+    }
+}
