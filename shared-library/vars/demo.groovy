06ca7e089dbdd0a4d60bc117d62eb320cc925e7a
prabhavith <you@example.com>
2024-01-28 14:57:09 +0530

    pushed jenkins file

--- shared-library/vars/demo.groovy
+++ shared-library/vars/demo.groovy
@@ -1,24 +1,18 @@
 // check README.md file in git repo for more clarity.
 def call() {
     node {
+        stage('code checkout') {
+            echo 'code checkout'
+        }
+        stage('compile') {
+            echo 'compile'
+        }
         sh 'env'
         if (env.BRANCH_NAME == "main") {
-            stage('code checkout') {
-                echo 'code checkout'
-            }
-            stage('compile') {
-                echo 'compile'
-            }
             stage('build') {
                 echo 'build'
             }
         } else if (env.BRANCH_NAME ==~ "PR.*" ) {
-            stage('code checkout') {
-                echo 'code checkout'
-            }
-            stage('compile') {
-                echo 'compile'
-            }
             stage('Unit test cases') {
                 echo 'unit test cases'
             }
@@ -27,12 +21,6 @@ def call() {
             }
         }
         else if (env.BRANCH_NAME ==~ "v..*") {
-            stage('code checkout') {
-                echo 'code checkout'
-            }
-            stage('compile') {
-                echo 'compile'
-            }
             stage('build') {
                 echo 'build'
             }
@@ -41,12 +29,6 @@ def call() {
             }
         }
         else {
-            stage('code checkout') {
-                echo 'code checkout'
-            }
-            stage('compile') {
-                echo 'compile'
-            }
             stage('Unit test cases') {
                 echo 'unit test cases'
             }
